aba6f9e646246725b3692ecef156410942c59d85
Zoha Ahmed <121173850+ZohaAhmed13@users.noreply.github.com>
2023-03-07 12:10:39 +0530

    Update view.php

--- views/MediaGateway/view.php
+++ views/MediaGateway/view.php
@@ -15,21 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'ID' => $model->ID], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'ID' => $model->ID], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'ID',
+            'id',
             'Name',
             'SoftSwitch',
         ],
